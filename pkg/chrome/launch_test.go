d35474ed7eeff2b28cc523bf67c828cfa67ab380
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2025-03-18 19:21:44 -0400

    update

---
+++ pkg/chrome/launch_test.go
@@ -0,0 +1,135 @@
+package chrome
+
+import (
+	"context"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"slices"
+	"testing"
+	"time"
+)
+
+func TestLaunchChrome(t *testing.T) {
+	ctx := context.Background()
+	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	expected := "Launching Chrome from /Applications/Google Chrome.app/Contents/MacOS/Google Chrome on port 9222"
+
+	options, err := NewOptions(&ctx, chromePath, false, "", "", false)
+	if err != nil {
+		t.Errorf("NewOptions() error = %v", err)
+		return
+	}
+	got, err := LaunchChrome("https://www.google.com", options)
+	if err != nil {
+		t.Errorf("LaunchChrome() error = %v", err)
+		return
+	}
+	if got.Opts.GetChromePath() != expected {
+		t.Errorf("LaunchChrome() = %q, want %q", got.Opts.GetChromePath(), expected)
+	}
+}
+
+func TestGetChromePath(t *testing.T) {
+	path, err := GetChromePath()
+	if err != nil {
+		t.Errorf("GetChromePath() error = %v", err)
+		return
+	}
+
+	// Verify the path exists
+	if _, err := os.Stat(path); err != nil {
+		t.Errorf("Chrome path does not exist: %v", err)
+	}
+
+	// Verify the path is correct for the current OS
+	switch runtime.GOOS {
+	case "darwin":
+		if path != "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" &&
+			path != "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary" {
+			t.Errorf("Invalid Chrome path for macOS: %s", path)
+		}
+	case "windows":
+		expectedPaths := []string{
+			filepath.Join(os.Getenv("LOCALAPPDATA"), "Google", "Chrome", "Application", "chrome.exe"),
+			filepath.Join(os.Getenv("PROGRAMFILES"), "Google", "Chrome", "Application", "chrome.exe"),
+			filepath.Join(os.Getenv("PROGRAMFILES(X86)"), "Google", "Chrome", "Application", "chrome.exe"),
+		}
+		validPath := false
+		for _, expectedPath := range expectedPaths {
+			if path == expectedPath {
+				validPath = true
+				break
+			}
+		}
+		if !validPath {
+			t.Errorf("Invalid Chrome path for Windows: %s", path)
+		}
+	}
+}
+
+func TestLaunchChromeWithArgs(t *testing.T) {
+	ctx := context.Background()
+	chromePath, err := GetChromePath()
+	if err != nil {
+		t.Errorf("GetChromePath() error = %v", err)
+		return
+	}
+	headless := true
+	options, err := NewOptions(&ctx, chromePath, headless, "142.173.80.190:5190:MZeH5aeTIh:CwEOKuP6Ca", "", true)
+	if err != nil {
+		t.Errorf("NewOptions() error = %v", err)
+		return
+	}
+	browser, err := LaunchChrome("", options, []string{})
+	if err != nil {
+		t.Errorf("LaunchChrome() error = %v", err)
+		return
+	}
+	if browser.Opts.GetChromePath() != chromePath {
+		t.Errorf("LaunchChrome() = %q, want %q", browser.Opts.GetChromePath(), chromePath)
+	}
+	if browser.Opts.GetHeadless() != headless {
+		t.Errorf("LaunchChrome() = %t, want %t", browser.Opts.GetHeadless(), headless)
+	}
+	if !slices.Contains(browser.Opts.GetArgs(), "--no-first-run") {
+		t.Errorf("LaunchChrome() = %q, want %q", browser.Opts.GetArgs(), []string{"--no-first-run"})
+	}
+	defer browser.Close()
+
+	err = browser.CurrentPage.Navigate("https://www.ticketmaster.com/event/0000617D0901855C")
+	if err != nil {
+		t.Errorf("Navigate() error = %v", err)
+		return
+	}
+
+	var tmptCookie *Cookie
+	for range 10 {
+		cookies, err := browser.CurrentPage.GetCookies()
+		if err != nil {
+			t.Errorf("GetCookies() error = %v", err)
+			return
+		}
+		if len(cookies) == 0 {
+			t.Errorf("GetCookies() came back empty")
+		}
+		var hasTmpt bool
+		for _, cookie := range cookies {
+			if cookie.Name == "tmpt" {
+				tmptCookie = cookie
+				hasTmpt = true
+				break
+			}
+		}
+		if hasTmpt {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	if tmptCookie == nil {
+		t.Errorf("GetCookies() did not return TMPSession cookie")
+	}
+	log.Println(tmptCookie.Value)
+
+}
