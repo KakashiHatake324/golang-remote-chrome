d263d47fd0c56078b9cf9f83129dd91c7dcc9c22
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2025-03-19 19:30:45 -0400

    Update launch.go

--- pkg/chrome/launch.go
+++ pkg/chrome/launch.go
@@ -155,10 +155,13 @@ func LaunchChrome(startUrl string, opts *Options, argsOpts ...[]FlagType) (*Brow
 		page.EnableFetch()
 	}
 	page.EnableNetwork()
-	command := browser.GetCurrentPage().waitLoad()
-	if err := browser.GetCurrentPage().send(command); err != nil {
-		return nil, fmt.Errorf("failed to send command: %v", err)
-	}
+
+	/*
+		command := browser.GetCurrentPage().waitLoad()
+		if err := browser.GetCurrentPage().send(command); err != nil {
+			return nil, fmt.Errorf("failed to send command: %v", err)
+		}
+	*/
 
 	if startUrl != "" {
 		if err := browser.GetCurrentPage().Navigate(startUrl); err != nil {
